3c21549ee93d89b5b0c626249b3fa28a863459ac
Eryn Wells <eryn@vmware.com>
2013-08-29 15:05:38 -0700

    Bare bones tests module

---
+++ tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Eryn Wells <eryn@erynwells.me>
+
+'''
+Tests for sudoku.
+'''
+
+import nose
+import sudoku
+import unittest
+
+
+def main():
+    nose.main()
+
+
+if __name__ == '__main__':
+    main()
