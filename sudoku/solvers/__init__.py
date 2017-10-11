77456d51142e819c68247dcdd81eae50654d1e0c
Eryn Wells <eryn@erynwells.me>
2017-10-11 16:07:33 -0700

    Add solvers package

---
+++ sudoku/solvers/__init__.py
@@ -0,0 +1,8 @@
+# Eryn Wells <eryn@erynwells.me>
+'''
+'''
+
+class Solver:
+    def solve(self, sudoku):
+        # TODO: Any subclass of Solver should implement this.
+        raise NotImplementedError
